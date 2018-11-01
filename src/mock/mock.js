e4df5996521d6a48292988e16d40d18c1d82b072
hankk <hankuikuide@163.com>
2018-11-01 17:25:11 +0800

    框架基本搭建完成
    框架基本搭建完成

---
+++ src/mock/mock.js
@@ -0,0 +1,73 @@
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import {
+  LoginUsers,
+  Users
+} from './data/user'
+let _User = Users
+
+export default {
+  bootstrap () {
+    let mock = new MockAdapter(axios)
+
+    // Login
+    mock.onPost('/login').reply(config => {
+      let {
+        username,
+        password
+      } = JSON.parse(config.data)
+
+      return new Promise((resolve, reject) => {
+        let user = null
+
+        setTimeout(() => {
+          let hasUser = LoginUsers.some(u => {
+            if (u.username === username && u.password === password) {
+              user = JSON.parse(JSON.stringify(u))
+              user.password = undefined
+              return true
+            }
+          })
+
+          if (hasUser) {
+            resolve([200, {
+              code: 200,
+              msg: '请求成功',
+              user
+            }])
+          } else {
+            resolve([200, {
+              code: 500,
+              msg: '账号或密码错误'
+            }])
+          }
+        }, 1000)
+      })
+    })
+
+    // User List
+    mock.onGet('/user/listpage').reply(config => {
+      let {
+        page,
+        name
+      } = config.params
+
+      let mockUsers = _User.filter(user => {
+        if (name && user.name.indexOf(name) === -1) {
+          return false
+        }
+        return true
+      })
+      let totoal = mockUsers.length
+      mockUsers = mockUsers.filter((u, index) => index < 20 * page && index >= 20 * (page - 1))
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve([200, {
+            totoal: totoal,
+            users: mockUsers
+          }])
+        }, 1000)
+      })
+    })
+  }
+}
---
+++ src/routes.js
@@ -0,0 +1,40 @@
+import Login from './views/Login.vue'
+import Home from './views/Home.vue'
+import Table from './views/nav1/Table.vue'
+
+let routes = [{
+  path: '/login',
+  component: Login,
+  name: '',
+  hidden: true
+}, {
+  path: '/',
+  component: Home,
+  name: '原生控件',
+  iconCls: 'el-icon-location',
+  children: [{
+    path: '/table',
+    component: Table,
+    name: '表格'
+  }, {
+    path: '/date',
+    component: Table,
+    name: '日期'
+  }]
+}, {
+  path: '/',
+  component: Home,
+  name: '自定义控件',
+  iconCls: 'el-icon-menu',
+  children: [{
+    path: '/dropdowntree',
+    component: Table,
+    name: '表格'
+  }, {
+    path: '/dropdowntable',
+    component: Table,
+    name: '日期'
+  }]
+}]
+
+export default routes
