7ef1e70ffd93480b37067451eaa5cff98ec7f99c
hankuikuide <hankuikuide@163.com>
2019-04-20 01:09:24 +0800

    group mananger pemisson

--- src/views/sys/group/GroupManager.vue
+++ src/views/sys/group/GroupManager.vue
@@ -34,8 +34,28 @@
         <el-form-item label="所选择组：" prop="name">{{ groupName }}
         </el-form-item>
         <el-tabs :tab-position="tabPosition" style="height: 200px;">
-          <el-tab-pane label="包含用户">包含用户</el-tab-pane>
-          <el-tab-pane label="所属角色">所属角色</el-tab-pane>
+          <el-tab-pane label="包含用户">
+          <!-- <el-form-item label="包含用户" prop="name">
+          </el-form-item> -->
+               <!--列表-->
+          <el-table :data="users" :row-style="{height:0}" :header-row-style="{height:0}" :header-cell-style="{padding:0}" :cell-style="{padding:0}" highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;">
+            <el-table-column type="selection" width="45">
+            </el-table-column>
+            <el-table-column prop="name" label="用户名" width="100" sortable>
+            </el-table-column>
+            <el-table-column prop="userName" label="姓名" width="100" sortable>
+            </el-table-column>
+            <el-table-column prop="email" label="邮箱" min-width="200" sortable>
+            </el-table-column>
+            <el-table-column prop="lastDate" label="最近登录时间" width="180" sortable>
+            </el-table-column>
+            <el-table-column prop="loginCount" label="登录次数" width="120" sortable>
+            </el-table-column>
+          </el-table>
+          </el-tab-pane>
+          <el-tab-pane label="所属角色">所属角色
+            <tree-table :data="rowData" :columns="columns" border/>
+          </el-tab-pane>
           <el-tab-pane label="组权限">组权限</el-tab-pane>
           <el-tab-pane label="总权限">总权限</el-tab-pane>
         </el-tabs>
@@ -49,17 +69,20 @@
   </section>
 </template>
 <script>
+import Mock from 'mockjs'
+import treeTable from '../../../components/TreeTable'
 import { getTreeLabel } from '../../../util/util'
 export default {
   data () {
     return {
-      groupName: '开发一部 / 一组',
+      groupName: '',
       tabPosition: 'left',
       filters: {
         name: '',
         recipe: ''
       },
       sels: [],
+      listLoading: false,
       addLoading: false,
       addFormRules: {
         name: [{
@@ -68,6 +91,129 @@ export default {
           trigger: 'blur'
         }]
       },
+      columns: [
+        {
+          text: '事件',
+          value: 'event',
+          width: 200
+        },
+        {
+          text: 'ID',
+          value: 'id'
+        }
+      ],
+      rowData: [
+        {
+          id: 0,
+          event: '事件1',
+          timeLine: 50,
+          comment: '无'
+        }, {
+          id: 1,
+          event: '事件1',
+          timeLine: 100,
+          comment: '无',
+          children: [
+            {
+              id: 2,
+              event: '事件2',
+              timeLine: 10,
+              comment: '无'
+            },
+            {
+              id: 3,
+              event: '事件3',
+              timeLine: 90,
+              comment: '无',
+              children: [
+                {
+                  id: 4,
+                  event: '事件4',
+                  timeLine: 5,
+                  comment: '无'
+                },
+                {
+                  id: 5,
+                  event: '事件5',
+                  timeLine: 10,
+                  comment: '无'
+                },
+                {
+                  id: 6,
+                  event: '事件6',
+                  timeLine: 75,
+                  comment: '无',
+                  children: [
+                    {
+                      id: 7,
+                      event: '事件7',
+                      timeLine: 50,
+                      comment: '无',
+                      children: [
+                        {
+                          id: 71,
+                          event: '事件71',
+                          timeLine: 25,
+                          comment: 'xx'
+                        },
+                        {
+                          id: 72,
+                          event: '事件72',
+                          timeLine: 5,
+                          comment: 'xx'
+                        },
+                        {
+                          id: 73,
+                          event: '事件73',
+                          timeLine: 20,
+                          comment: 'xx'
+                        }
+                      ]
+                    },
+                    {
+                      id: 8,
+                      event: '事件8',
+                      timeLine: 25,
+                      comment: '无'
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      users: [{
+        name: Mock.Random.first(),
+        userName: Mock.Random.cname(),
+        email: Mock.Random.email(),
+        lastDate: Mock.Random.datetime(),
+        loginCount: Mock.Random.integer(60, 100)
+      }, {
+        name: Mock.Random.first(),
+        userName: Mock.Random.cname(),
+        email: Mock.Random.email(),
+        lastDate: Mock.Random.datetime(),
+        loginCount: Mock.Random.integer(60, 100)
+      }, {
+        name: Mock.Random.first(),
+        userName: Mock.Random.cname(),
+        email: Mock.Random.email(),
+        lastDate: Mock.Random.datetime(),
+        loginCount: Mock.Random.integer(60, 100)
+      }, {
+        name: Mock.Random.first(),
+        userName: Mock.Random.cname(),
+        email: Mock.Random.email(),
+        lastDate: Mock.Random.datetime(),
+        loginCount: Mock.Random.integer(60, 100)
+      }, {
+        name: Mock.Random.first(),
+        userName: Mock.Random.cname(),
+        email: Mock.Random.email(),
+        lastDate: Mock.Random.datetime(),
+        loginCount: Mock.Random.integer(60, 100)
+      }],
       data: [{
         id: 1,
         label: '技术中心',
@@ -109,13 +255,18 @@ export default {
       }
     }
   },
+  components: { treeTable },
   methods: {
     handleNodeClick (node, data, value) {
-      this.groupName = getTreeLabel(data)
-      console.dir(data)
+      let name = getTreeLabel(data)
+      let startIndex = name.indexOf('/') + 1
+      this.groupName = name.substring(startIndex)
     },
     handleAdd () {
 
+    },
+    selsChange (sels) {
+      this.sels = sels
     },
     getUsers () {
 
